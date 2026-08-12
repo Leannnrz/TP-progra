48454e3c5dde297eba0387121c053b4180544625
SantiagoCos <sacostilla@uade.edu.ar>
2026-08-12 15:28:22 -0300

    Add files via upload

---
+++ TP Programacion.py
@@ -0,0 +1,7 @@
+# Listas de clientes
+id_cliente = ["C001", "C002", "C003", "C004", "C005", "C006", "C007", "C008", "C009", "C010"]
+nombre_cliente = ["Juan Perez", "Maria Gomez", "Carlos Lopez", "Ana Martinez", "Luis Fernandez", "Sofia Torres", "Diego Ramirez", "Valentina Castro", "Martin Rojas", "Lucia Diaz"]
+edad_cliente = [25, 42, 31, 55, 19, 38, 47, 29, 61, 34]
+tipo_cobertura = [1, 2, 1, 2, 1, 2, 1, 1, 2, 2]
+
+print(edad_cliente) 
